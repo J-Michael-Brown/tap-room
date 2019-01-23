f004684a51f6c33fb5e6f8769151922a05e1ea44
Christopher Cahill and Michael Brown <christopher@>
2019-01-23 14:25:04 -0800

    initialize repository

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Keg } from './models/keg.model'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tapRoom';
+
+  kegs: Keg[] = [
+    new Keg('lager A', 'someBrand', 100, 5, 6.5, 124),
+    new Keg('lager B', 'someOtherBrand', 112, 4, 9.0, 124),
+    new Keg('lager C', 'someBrandFrom Over there', 105, 6, 4.5, 124),
+  ]
+
 }
---
+++ src/app/models/keg.model.ts
@@ -0,0 +1,4 @@
+export class Keg {
+  constructor(public name: string, public brand: string, public pricePerKeg: number, public pricePerPint: number, public abv: number, public pintsLeft: number)  { }
+
+}
